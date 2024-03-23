acc00c43961eedda705d79889537b9d503c75acb
Hunvely <jihoon9611@gmail.com>
2024-03-24 01:27:43 +0900

    add menu box code

--- nextjs-14/app/page.tsx
+++ nextjs-14/app/page.tsx
@@ -14,17 +14,31 @@ export default function Home() {
     <div className={styles.container}>
        <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
         <div>
-          <Link href="/login" className={styles.container}>Sign in</Link>
+          <Link href="/login" className={styles.link}>Sign in |</Link>
         </div>
         <div>
-          <Link href="/join" className={styles.container}>Sign up</Link>
+          <Link href="/join" className={styles.link}>| Sign up</Link>
         </div>
       </div>
-      <h1 className={styles.title}>Whisky</h1>
+      <h1 className={styles.title}>Whiskey</h1>
       <p className={styles.subtitle}>Start on your journey.</p>
       <div className={styles.description}>
-        <p>Whisky is a liquor with a assortment of scents and flavors.</p>
+        <p>Whiskey is a liquor with a assortment of scents and flavors.</p>
         <p>It is produced all over the world, and tastes shift depending on region, ingredients, and manufacturing process.</p>
+      </div>
+      <hr></hr>
+
+      <div className={styles.gridContainer}>
+        <Link href="" passHref><div className={styles.gridItem}>whiskey story</div></Link>
+        <Link href="" passHref><div className={styles.gridItem}>type of whiskey</div></Link>
+        <Link href="" passHref><div className={styles.gridItem}>recommended whiskey</div></Link>
+        <Link href="" passHref><div className={styles.gridItem}>whiskey store</div></Link>
+        <Link href="" passHref><div className={styles.gridItem}>whiskey board</div></Link>
+        <Link href="" passHref><div className={styles.gridItem}>event whiskey</div></Link>
+      </div>
+      <hr></hr>
+
+      <div className={styles.footer}>
       </div>
     </div>
   );
