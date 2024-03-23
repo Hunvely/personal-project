994c24fd26aa4f80bcc76948f1d28065291b889c
Hunvely <jihoon9611@gmail.com>
2024-03-23 23:05:53 +0900

    mainpage code edit

--- nextjs-14/app/page.tsx
+++ nextjs-14/app/page.tsx
@@ -13,18 +13,18 @@ export default function Home() {
   
     <div className={styles.container}>
        <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-        <div style={{ marginRight: '10px' }}>
-          <Link href="/login">로그인</Link>
+        <div>
+          <Link href="/login" className={styles.container}>Sign in</Link>
         </div>
         <div>
-          <Link href="/join">회원가입</Link>
+          <Link href="/join" className={styles.container}>Sign up</Link>
         </div>
       </div>
-      <h1 className={styles.title}>Whiskey</h1>
-      <p className={styles.subtitle}>당신의 위스키 여정을 시작하세요.</p>
+      <h1 className={styles.title}>Whisky</h1>
+      <p className={styles.subtitle}>Start on your journey.</p>
       <div className={styles.description}>
-        <p>위스키는 다양한 향과 맛을 즐길 수 있는 음료입니다.</p>
-        <p>위스키는 세계 각지에서 생산되고 있으며, 그 맛의 차이는 지역, 재료, 제조 과정에 따라 다릅니다.</p>
+        <p>Whisky is a liquor with a assortment of scents and flavors.</p>
+        <p>It is produced all over the world, and tastes shift depending on region, ingredients, and manufacturing process.</p>
       </div>
     </div>
   );
