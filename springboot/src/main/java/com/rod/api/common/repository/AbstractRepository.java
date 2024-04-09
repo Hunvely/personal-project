a8fa0626d7a67070f5a99d10244d15aea9010fe7
Hunvely <jihoon9611@gmail.com>
2024-04-09 17:19:10 +0900

    feat - Create AbstractRepository in common repository

--- springboot/src/main/java/com/rod/api/common/repository/AbstractRepository.java
+++ springboot/src/main/java/com/rod/api/common/repository/AbstractRepository.java
@@ -1,4 +1,4 @@
-package com.rod.api.common;
+package com.rod.api.common.repository;
 
 import java.io.IOException;
 import java.util.Map;
@@ -6,5 +6,4 @@ import java.util.Map;
 public abstract class AbstractRepository {
 
     public abstract Map<String, ?> save(Map<String, ?> paramMap) throws IOException;
-    public abstract Map<String, ?> save2(Map<String, ?> paramMap) throws IOException;
 }
