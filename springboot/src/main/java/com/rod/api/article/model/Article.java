5de68872bd0c45d29de487be992e2feb2da63ef7
Hunvely <jihoon9611@gmail.com>
2024-04-11 15:24:01 +0900

    feat - Existing Article were divided into Entity and DTO

--- springboot/src/main/java/com/rod/api/article/model/Article.java
+++ springboot/src/main/java/com/rod/api/article/model/Article.java
@@ -1,7 +1,7 @@
-package com.rod.api.article;
+package com.rod.api.article.model;
 
 import com.rod.api.board.Board;
-import com.rod.api.user.User;
+import com.rod.api.user.model.User;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -9,30 +9,24 @@ import lombok.*;
 @Table(name = "articles")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@Builder
+@AllArgsConstructor
 @ToString(exclude = {"id"})
 public class Article {
     @Id
-    @Column(name="article_id", nullable = false)
+    @Column(name = "article_id", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String content;
-    @Column(name = "register_date")
-    private String registerDate;
 
     @ManyToOne
     @JoinColumn(name = "board_id")
     private Board board;
+
     @ManyToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "writer_id")
     private User writer;
 
-    @Builder(builderMethodName = "builder")
-    public Article(String title, String content, User writer, String registerDate) {
-        this.title = title;
-        this.content = content;
-        this.writer = writer;
-        this.registerDate = registerDate;
 
-    }
 }
---
+++ springboot/src/main/java/com/rod/api/article/model/ArticleDto.java
@@ -0,0 +1,22 @@
+package com.rod.api.article.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ArticleDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String registerDate;
+    private Long writerId;
+    private Long boardId;
+}
