4ff31a148458dfd8568c8ecd986a94b7bda9a31f
Hunvely <jihoon9611@gmail.com>
2024-04-11 15:25:38 +0900

    feat - Add method to convert between DTO and Entity

---
+++ springboot/src/main/java/com/rod/api/article/service/ArticleService.java
@@ -0,0 +1,28 @@
+package com.rod.api.article.service;
+
+
+import com.rod.api.article.model.Article;
+import com.rod.api.article.model.ArticleDto;
+import com.rod.api.common.service.CommandService;
+import com.rod.api.common.service.QueryService;
+
+import java.util.List;
+
+public interface ArticleService extends CommandService<ArticleDto>, QueryService<ArticleDto> {
+
+    default Article dtoToEntity(ArticleDto articleDto) {
+        return Article.builder()
+                .id(articleDto.getId())
+                .title(articleDto.getTitle())
+                .content(articleDto.getContent())
+                .build();
+    }
+
+    default ArticleDto entityToDto(Article article) {
+        return ArticleDto.builder()
+                .id(article.getId())
+                .title(article.getTitle())
+                .content(article.getContent())
+                .build();
+    }
+}
