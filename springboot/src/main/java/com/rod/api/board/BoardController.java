db1e005032bebbd3db97da852b4503dc1478846a
Hunvely <jihoon9611@gmail.com>
2024-04-13 19:07:33 +0900

    feat - By adding annotations, controller ready to communicate with the client.

--- springboot/src/main/java/com/rod/api/board/BoardController.java
+++ springboot/src/main/java/com/rod/api/board/BoardController.java
@@ -1,13 +1,25 @@
 package com.rod.api.board;
 
+import com.rod.api.board.service.BoardServiceImpl;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:3000/")
-@RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/api/users")
+@Slf4j
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "400", description = "Invalid ID supplied"),
+        @ApiResponse(responseCode = "404", description = "Customer not found")})
 public class BoardController {
 
     private BoardServiceImpl boardService;
+
+
 }
