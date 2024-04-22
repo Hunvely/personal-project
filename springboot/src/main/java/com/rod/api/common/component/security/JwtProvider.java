9df0a7a97cac4367ab2c44e1c92404a7dc509e89
Hunvely <jihoon9611@gmail.com>
2024-04-22 21:46:41 +0900

    feat - Add method to extract JWT token from Authorization header & Implement method to extract JWT payload from token

--- springboot/src/main/java/com/rod/api/common/component/security/JwtProvider.java
+++ springboot/src/main/java/com/rod/api/common/component/security/JwtProvider.java
@@ -1,10 +1,11 @@
-package com.rod.api.common.component;
+package com.rod.api.common.component.security;
 
 
 import com.rod.api.user.model.UserDto;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 import javax.crypto.SecretKey;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Base64;
 import java.util.Date;
 
 
@@ -24,6 +26,7 @@ public class JwtProvider {
     public final SecretKey secretKey;
 
     Instant expiredDate = Instant.now().plus(1, ChronoUnit.DAYS);
+
     public JwtProvider(@Value("${jwt.secret}") String secretKey) {
         this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(secretKey));
     }
@@ -45,4 +48,27 @@ public class JwtProvider {
     }
 
 
+    public String extractTokenFromHeader(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+
+        if (bearerToken != null && bearerToken.startsWith("bearer")) {
+            return bearerToken.substring(7);
+        }
+
+        return null;
+    }
+
+    public static String getPayload(String token) {
+        String[] chunks = token.split("\\.");
+        Base64.Decoder decoder = Base64.getDecoder();
+
+        String header = new String(decoder.decode(chunks[0]));
+        String payload = new String(decoder.decode(chunks[1]));
+
+        log.info("Token header : " + header);
+        log.info("Token payload : " + payload);
+
+        // return new StringBuilder().append(header).append(payload).toString();
+        return payload;
+    }
 }
