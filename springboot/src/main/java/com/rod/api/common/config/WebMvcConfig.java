f09dededaa295bc2ac92916a85dcdfce0f72347a
Hunvely <jihoon9611@gmail.com>
2024-04-23 21:58:23 +0900

    feat - Add logic that does not perform token verification for the path to process login requests.

--- springboot/src/main/java/com/rod/api/common/config/WebMvcConfig.java
+++ springboot/src/main/java/com/rod/api/common/config/WebMvcConfig.java
@@ -13,9 +13,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     private final AuthInterceptor authInterceptor;
 
     @Override
-    public void addInterceptors(InterceptorRegistry registry) {
+    public void addInterceptors(InterceptorRegistry registry) { // 인터셉터 등록
         registry.addInterceptor(authInterceptor) // Servlet 컨테이너의 객체를 Spring 컨테이너와 연결
-                .excludePathPatterns("/favion.ico")
-                .addPathPatterns("/api/**");
+                .excludePathPatterns("/api/auth/**") // 로그인 할 때는 토큰이 없기 때문에 인터셉터를 제거해야 함.
+                .addPathPatterns("/api/**"); // /api/로 시작하는 경로에 대한 모든 요청을 인터셉트
     }
 }
