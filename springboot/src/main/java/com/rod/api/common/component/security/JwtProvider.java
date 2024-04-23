39e206a33cc253e332762baf396e8e55d2695018
Hunvely <jihoon9611@gmail.com>
2024-04-23 22:02:09 +0900

    feat - Add printPayload method prints the token's header and payload as a log & getPayload method returns the payload.

--- springboot/src/main/java/com/rod/api/common/component/security/JwtProvider.java
+++ springboot/src/main/java/com/rod/api/common/component/security/JwtProvider.java
@@ -2,6 +2,7 @@ package com.rod.api.common.component.security;
 
 
 import com.rod.api.user.model.UserDto;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -39,7 +40,7 @@ public class JwtProvider {
                 .expiration(Date.from(expiredDate))
                 .subject("rod")
                 .claim("username", userDto.getUsername())
-                .claim("userId", userDto.getId())
+                .claim("id", userDto.getId())
                 .compact();
 
         log.info("로그인 성공으로 발급된 토큰 : " + token);
@@ -49,26 +50,27 @@ public class JwtProvider {
 
 
     public String extractTokenFromHeader(HttpServletRequest request) {
+        log.info("프론트에서 넘어온 Request 값 : {}", request.getServletPath());
         String bearerToken = request.getHeader("Authorization");
+        log.info("프론트에서 넘어온 토큰 값 : {}", bearerToken);
 
-        if (bearerToken != null && bearerToken.startsWith("bearer")) {
-            return bearerToken.substring(7);
-        }
-
-        return null;
+        return bearerToken != null && bearerToken.startsWith("Bearer ") ? bearerToken.substring(7) : "undefined";
     }
 
-    public static String getPayload(String token) {
+    public static void printPayload(String token) {
         String[] chunks = token.split("\\.");
         Base64.Decoder decoder = Base64.getDecoder();
 
         String header = new String(decoder.decode(chunks[0]));
         String payload = new String(decoder.decode(chunks[1]));
 
-        log.info("Token header : " + header);
-        log.info("Token payload : " + payload);
+        log.info("JWT Provider Access Token header : " + header);
+        log.info("JWT Provider Access Token payload : " + payload);
 
         // return new StringBuilder().append(header).append(payload).toString();
-        return payload;
+    }
+
+    public Claims getPayload(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload(); // 정형화
     }
 }
