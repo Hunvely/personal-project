ce82f4ffb5a3b59192052174b674ced141081eba
Hunvely <jihoon9611@gmail.com>
2024-04-25 19:51:30 +0900

    feat - delete import

--- nextjs-14/app/components/board/service/board-service.ts
+++ nextjs-14/app/components/board/service/board-service.ts
@@ -3,7 +3,7 @@ import { IBoard } from "../model/board";
 import { findAllBoardsAPI, findBoardByIdAPI } from "./board-api";
 import axios from "axios";
 import { API } from "@/app/components/common/enums/API";
-import AxiosConfig, { instance } from "@/app/components/common/configs/axios-config";
+import AxiosConfig from "@/app/components/common/configs/axios-config";
 
 export const findAllBoards: any = createAsyncThunk(
     'boards/findAllBoards',
