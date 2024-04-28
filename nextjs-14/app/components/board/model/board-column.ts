3e4b578835ff66e066828a209d362cf00c6bbfce
Hunvely <jihoon9611@gmail.com>
2024-04-28 18:12:09 +0900

    feat - change column

--- nextjs-14/app/components/board/model/board-column.ts
+++ nextjs-14/app/components/board/model/board-column.ts
@@ -1,7 +1,7 @@
 export interface BoardColumn{
-    id? : number  // ?는 자바의 Optional 없어도 된다라는 뜻
-    boardName? : string
-    boardType? : string
-    regDate?: string
-    modDate?: string
+    id? : number
+    title? : string
+    description? : string
+    regDate? : string
+    modDate? : string
 }
